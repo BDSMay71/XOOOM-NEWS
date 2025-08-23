ae70aef9c083a9f1dad949fedfdcfd8596d34147
BDSMay71 <thebriansteel@gmail.com>
2025-08-23 08:16:07 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import NewsGrid from './components/NewsGrid';
 
 export default async function Page() {
   const buckets = await fetchAllFeeds();
+
   return (
     <main className="container" style={{ paddingBlock: '1.25rem' }}>
       <h1 className="pageTitle">XOOOM news from the world to your street</h1>
